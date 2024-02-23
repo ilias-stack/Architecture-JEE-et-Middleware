a2021fd5beb5c049b30d3fe35fc8086eace222c1
ilias-stack <pay.ilias2002@gmail.com>
2024-02-23 20:21:37 +0100

    XML injection implemented

--- TP1/src/main/java/part2/mini_injection_framework/context/XMLContextImpl.java
+++ TP1/src/main/java/part2/mini_injection_framework/context/XMLContextImpl.java
@@ -3,9 +3,9 @@ package part2.mini_injection_framework.context;
 import part2.mini_injection_framework.core.Beans;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 
 public class XMLContextImpl extends BaseContext {
 
@@ -15,17 +15,40 @@ public class XMLContextImpl extends BaseContext {
             try {
                 this.handleXMLFile(fileStream);
                 return;
-            } catch (JAXBException e) {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
         throw new RuntimeException("Configuration file location is not found inside resources");
     }
 
-    private void handleXMLFile(InputStream inputStream) throws JAXBException {
+    private void handleXMLFile(InputStream inputStream) throws Exception {
         JAXBContext context = JAXBContext.newInstance(Beans.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
-        Beans allBeans = (Beans) unmarshaller.unmarshal(inputStream);
-        System.out.println(allBeans.getBeans().get(1).getProperties());
+
+        // Getting all the beans in the list
+        var allBeans = ((Beans) unmarshaller.unmarshal(inputStream)).getBeans();
+        for (var thisBean : allBeans){
+            String beanId = thisBean.getId();
+            Class<?> clazz = Class.forName(thisBean.getClassName());
+            var beanInstance = clazz.getConstructor().newInstance();
+            this.beans.put(beanId.isEmpty()? thisBean.getClassName():beanId,beanInstance);
+
+            var props = thisBean.getProperties();
+            if(props==null)continue;
+
+            // Setting the fields via properties
+            var fields = clazz.getDeclaredFields();
+            for(var prop:props){
+                for (Field field:fields){
+                    if(field.getName().equals(prop.getName())){
+                        field.setAccessible(true);
+                        Object beanVal = null;
+                        if(!beanId.isEmpty()) beanVal = getBean(prop.getRef());
+                        field.set(beanInstance,beanVal);
+                    }
+                }
+            }
+        }
     }
 }
--- TP1/src/main/java/part2/presentation/XMLPresentation.java
+++ TP1/src/main/java/part2/presentation/XMLPresentation.java
@@ -1,11 +1,14 @@
 package part2.presentation;
 
+import part2.metier.IMetier;
 import part2.mini_injection_framework.context.IContext;
 import part2.mini_injection_framework.context.XMLContextImpl;
+import part2.mini_injection_framework.core.BeanException;
 
 public class XMLPresentation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeanException {
         IContext context = new XMLContextImpl("configuration.xml");
-//        beans
+        IMetier metier =(IMetier) context.getBean(IMetier.class);
+        System.out.println(metier.calcul());
     }
 }
