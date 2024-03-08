dbcd142917db448891a1477f5f33a4c22545dc8d
ilias-stack <pay.ilias2002@gmail.com>
2024-03-08 06:53:45 +0100

    Web application base

--- tp3/src/main/java/com/example/tp3/Tp3Application.java
+++ tp3/src/main/java/com/example/tp3/Tp3Application.java
@@ -1,7 +1,14 @@
 package com.example.tp3;
 
+import com.example.tp3.entities.Patient;
+import com.example.tp3.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class Tp3Application {
@@ -10,4 +17,13 @@ public class Tp3Application {
         SpringApplication.run(Tp3Application.class, args);
     }
 
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) throws Exception {
+        return args -> {
+            patientRepository.save(new Patient(null,"Ilias",new Date(),false,19));
+            patientRepository.save(new Patient(null,"Sanae",new Date(),true,9));
+            patientRepository.save(new Patient(null,"Mohammed",new Date(),false,17));
+        };
+    }
 }
