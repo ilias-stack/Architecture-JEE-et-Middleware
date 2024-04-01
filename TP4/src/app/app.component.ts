8bc9b6b28fe6dd54377f6ef635a4b8cf371286a4
ilias-stack <pay.ilias2002@gmail.com>
2024-04-01 22:00:26 +0000

    basic loop implemented

--- TP4/src/app/app.component.ts
+++ TP4/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'TP4';
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+  }
+
+  actions: Array<any> = [
+    { title: 'Home', route: '/home', icon: 'house' },
+    { title: 'Products', route: '/products', icon: 'search' },
+    { title: 'New Product', route: '/newProduct', icon: 'safe' },
+  ];
+
+  currentAction: any;
 }
