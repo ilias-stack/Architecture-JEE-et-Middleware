e7bb061d6123c66f4d37298798b3759f71ce3af3
Ilias Zaazaa <77403753+ilias-stack@users.noreply.github.com>
2024-05-07 16:48:58 +0100

    current page indicator

--- ebanking/ebanking-frontend/src/app/navbar/navbar.component.ts
+++ ebanking/ebanking-frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent{
+  constructor(public router: Router, private cdr: ChangeDetectorRef) {}
 
+  isRouteActive(route: string): boolean {
+    return this.router.url.includes(route);
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigateByUrl(route).then(() => {
+      this.cdr.detectChanges();
+    });
+  }
 }
