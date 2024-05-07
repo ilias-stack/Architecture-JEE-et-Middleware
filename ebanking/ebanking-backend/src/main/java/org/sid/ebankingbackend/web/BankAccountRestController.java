bc86ab509996a6107a95010652e6cba882b48c44
Ilias Zaazaa <77403753+ilias-stack@users.noreply.github.com>
2024-05-07 19:16:43 +0100

    backend secured

--- ebanking/ebanking-backend/src/main/java/org/sid/ebankingbackend/web/BankAccountRestController.java
+++ ebanking/ebanking-backend/src/main/java/org/sid/ebankingbackend/web/BankAccountRestController.java
@@ -5,6 +5,7 @@ import org.sid.ebankingbackend.dtos.*;
 import org.sid.ebankingbackend.exceptions.BalanceNotSufficientException;
 import org.sid.ebankingbackend.exceptions.BankAccountNotFoundException;
 import org.sid.ebankingbackend.service.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,26 +21,31 @@ public class BankAccountRestController {
 
 
     @GetMapping("/accounts/{accountId}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
         return bankAccountService.getBankAccount(accountId);
     }
 
     @GetMapping("/accounts")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public List<BankAccountDTO> getAllAccounts() throws BankAccountNotFoundException {
         return bankAccountService.bankAccountList();
     }
 
     @GetMapping("/accounts/{customerId}/accounts")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public List<BankAccountDTO> getAccounts(@PathVariable Long customerId){
         return bankAccountService.getCustomerAccounts(customerId);
     }
 
     @GetMapping("/accounts/{accountId}/operations")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
     }
 
     @GetMapping("/accounts/{accountId}/pageOperations")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public AccountHistoryDTO getAccountHistory(@PathVariable String accountId,
                                                @RequestParam(name = "page",defaultValue = "0") int page,
                                                @RequestParam(name = "size",defaultValue = "10") int size
@@ -48,18 +54,21 @@ public class BankAccountRestController {
     }
 
     @PostMapping("/accounts/debit")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(), debitDTO.getDescription());
         return debitDTO;
     }
 
     @PostMapping("/accounts/credit")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public CreditDTO debit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
         this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(), creditDTO.getDescription());
         return creditDTO;
     }
 
     @PostMapping("/accounts/transfer")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         this.bankAccountService.transfer(transferRequestDTO.getAccountSource(), transferRequestDTO.getAccountDestination(), transferRequestDTO.getAmount());
     }
--- ebanking/ebanking-backend/src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
+++ ebanking/ebanking-backend/src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.sid.ebankingbackend.dtos.CustomerDTO;
 import org.sid.ebankingbackend.exceptions.CustomerNotFoundException;
 import org.sid.ebankingbackend.service.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,32 +19,38 @@ public class CustomerRestController {
     private BankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
         return bankAccountService.searchCustomers(keyword);
     }
 
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_USER')")
     @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
     }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
         return bankAccountService.saveCustomer(customerDTO);
     }
 
     @PutMapping("/customers/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable(name = "id") Long customerId,@RequestBody CustomerDTO customerDTO){
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
 
     @DeleteMapping("/customers/{customerId}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public void deleteCustomer(@PathVariable Long customerId){
         bankAccountService.deleteCustomer(customerId);
     }
